58e482e5cc1ed4c5b7de764d516fb16d807951ee
adsionli <531020509@qq.com>
2018-05-24 10:22:31 +0800

    ljq

--- resources/views/Mail.blade.php
+++ resources/views/Mail.blade.php
@@ -1,78 +1,28 @@
 
-{{--<div style="margin:auto;background-color: rgb(245,245,241);height:800px;overflow: hidden;">--}}
-    {{--<div class="header">--}}
-        {{--<div>--}}
-            {{--<span class="title">AdsionLi -- 属于AdsionLi的个人站点</span>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="content">--}}
-        {{--<div class="content-title">--}}
-            {{--Hello,欢迎{{$name}}加入AdsionLi的个人站点进行交流！--}}
-            {{--<img src="http://p53z0yfgy.bkt.clouddn.com/jump.gif" alt="50*50" width="50px" height="50px">--}}
-        {{--</div>--}}
-        {{--<div class="content-email">--}}
-            {{--请点击这里  <a href="{{$url}}"><span style="color: red;">验证邮箱</span></a>--}}
-        {{--</div>--}}
-        {{--<div class="content-notify">--}}
-            {{--注意哦，有效时间只有6个小时，请抓紧！--}}
-            {{--<br>--}}
-            {{--<br>--}}
-            {{--<img src="http://p53z0yfgy.bkt.clouddn.com/cat.gif" alt="50*50" width="120px" height="120px">--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="foot">--}}
-        {{--<div class="foot-content">--}}
-            {{--© 2018 AdsionLi. Welcome to exchange.--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-{{--<style>--}}
-    {{--.header{--}}
-        {{--width:100%;--}}
-        {{--height: 60px;--}}
-        {{--text-align: center;--}}
-        {{--background-color:#000;--}}
-        {{--line-height: 60px;--}}
-        {{--color: #fff;--}}
-    {{--}--}}
-    {{--.title{--}}
-        {{--font-size: 20px;--}}
-        {{--font-weight: 600;--}}
-    {{--}--}}
-    {{--.content{--}}
-        {{--margin: auto;--}}
-        {{--width: 750px;--}}
-        {{--text-align: center;--}}
-    {{--}--}}
-    {{--.content-title{--}}
-        {{--font-size: 20px;--}}
-        {{--text-align: center;--}}
-        {{--margin-top: 60px;--}}
-    {{--}--}}
-    {{--.content-email{--}}
-        {{--font-size: 17px;--}}
-        {{--font-weight: 600;--}}
-        {{--margin-top: 20px;--}}
-    {{--}--}}
-    {{--.content-notify{--}}
-        {{--font-size: 16px;--}}
-        {{--font-weight: 600;--}}
-        {{--margin-top: 20px;--}}
-        {{--color: #000;--}}
-    {{--}--}}
-    {{--.foot{--}}
-        {{--position: absolute;--}}
-        {{--height: 60px;--}}
-        {{--width: 100%;--}}
-        {{--bottom: 0;--}}
-        {{--background-color: rgb(217,217,217);--}}
-        {{--text-align: center;--}}
-        {{--line-height: 60px;--}}
-    {{--}--}}
-    {{--.foot-content{--}}
-        {{--color: #777;--}}
-    {{--}--}}
-{{--</style>--}}
-{{$name}}，这是一封测试邮件
-<br>
-点击验证{{$url}}
+<div style="margin:auto;background-color: rgb(245,245,241);height:800px;overflow: hidden;">
+    <div style="width:100%;height: 60px;text-align: center;background-color:#000;line-height: 60px;color: #fff;">
+        <div>
+            <span style="font-size: 20px;font-weight: 600;">AdsionLi -- 属于AdsionLi的个人站点</span>
+        </div>
+    </div>
+    <div style="margin: auto;width: 750px;text-align: center;">
+        <div style="font-size: 20px;text-align: center;margin-top: 60px;">
+            Hello,欢迎{{$name}}加入AdsionLi的个人站点进行交流！
+            <img src="http://p53z0yfgy.bkt.clouddn.com/jump.gif" alt="50*50" width="50px" height="50px">
+        </div>
+        <div style="font-size: 17px;font-weight: 600;margin-top: 20px;">
+            请点击这里  <a href="{{$url}}"><span style="color: red;">验证邮箱</span></a>
+        </div>
+        <div style="font-size: 16px;font-weight: 600;margin-top: 20px;color: #000;">
+            注意哦，有效时间只有6个小时，请抓紧！
+            <br>
+            <br>
+            <img src="http://p53z0yfgy.bkt.clouddn.com/cat.gif" alt="50*50" width="120px" height="120px">
+        </div>
+    </div>
+    <div style="position: absolute;height: 60px;width: 100%;bottom: 0;background-color: rgb(217,217,217);text-align: center;line-height: 60px;">
+        <div style="color: #777;">
+            © 2018 AdsionLi. Welcome to exchange.
+        </div>
+    </div>
+</div>
