0e4b0218f740b9cb2726b46075f54986a66e113c
adsionli <531020509@qq.com>
2018-06-09 18:33:46 +0800

    ljq

--- app/Http/Controllers/ShirleyController.php
+++ app/Http/Controllers/ShirleyController.php
@@ -9,7 +9,7 @@ class ShirleyController extends Controller
 {
     public function login(Request $request){
         $user = User1::where('user_name',$request->get('user_name'))->get();
-        if($user->user_password == $request->get('user_password')){
+        if($user['user_password'] == $request->get('user_password')){
             return json_encode(['status' => "success",'user'=>$user]);
         }
         return json_encode(['status' => 'error','content'=>'错误']);
