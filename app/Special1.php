feb2a2ee95b321d2b4b3c13775ea819ad7f1b3a0
adsionli <531020509@qq.com>
2018-06-10 18:39:23 +0800

    ljq

--- app/Special1.php
+++ app/Special1.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Special1 extends Model
 {
-    protected $fillable = ['imgId','title','content','commentNum','likeNum'];
+    protected $fillable = ['imgId','title','content','commentNum','likeNum','user_id'];
 }
