a5c6ce5f36db115e57708e4a92163286eee4bb9d
adsionli <531020509@qq.com>
2018-07-25 14:36:25 +0800

    addFriend

---
+++ app/Http/Controllers/FirendController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+class FirendController extends Controller
+{
+    public function addFriend(){
+
+    }
+
+    public function selectFriend(){
+
+    }
+
+    public function delFriend(){
+
+    }
+
+    public function agreeFriend(){
+
+    }
+}
