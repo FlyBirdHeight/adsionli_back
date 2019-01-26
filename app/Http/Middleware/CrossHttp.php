4a7d41a371ce765aa81e06fc34c73691519b19cb
adsionli <531020509@qq.com>
2019-01-26 11:28:43 +0800

    ljq

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -8,14 +9,14 @@ class CrossHttp
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', config('crm.domain.cors'));
+        $response->header('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
